fed852082ced89322b2c1a3dfeebf1d6662876e8
jujuu <azizahnf85@gmail.com>
2026-04-09 10:10:04 +0700

    wassalam

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "https://dimsumwrap3d.berkahost.biz.id/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
